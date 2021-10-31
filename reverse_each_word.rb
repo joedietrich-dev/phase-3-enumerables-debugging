9d548d7d24274b31bbf7218854d4df5ecb30c4d5
joedietrich-dev <joe@joedietrich.net>
2021-10-31 15:46:37 -0400

    commit tree e6bfeabf1f60cc050dc9d65f51b9e9b682a67167

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,9 @@
-# Write your code here
+require 'pry'
+
+sentence = "Hello there, and how are you?"
+
+def reverse_each_word(sentence)
+  sentence.split.map( &:reverse ).join(" ")
+end
+
+puts reverse_each_word(sentence)
